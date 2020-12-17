cdb6e78665d115a9179279c3a48c3d652823a80e
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2020-12-16 22:48:31 -0300

    resolvendo a parte 2 e consertando a parte 1 do exercicio 2.2

--- modulo-4/ex-2.2/main.hs
+++ modulo-4/ex-2.2/main.hs
@@ -1,29 +1,48 @@
 -- 2.2 - Crie um tipo paramétrico para árvores binárias
 
--- 1. Crie uma árvore binária de booleanos.
--- 2. Crie uma árvore em que cada nó guarda uma lista de inteiros.
-
--- Leaf:
---  valor
---  dir
---  esq
-
 data Tree a = Leaf a (Tree a) (Tree a) | Empty deriving (Show)
 
-subtree4 :: Tree Int
-subtree4 = Leaf 5 Empty Empty
+-- 1. Crie uma árvore binária de booleanos.
+tree1 :: Tree Bool
+tree1 = Leaf True subtree11 subtree12
 
-subtree3 :: Tree Int
-subtree3 = Leaf 4 Empty Empty
+subtree11 :: Tree Bool
+subtree11 = Leaf True Empty Empty
 
-subtree2 :: Tree Int
-subtree2 = Leaf 3 subtree3 subtree4
+subtree12 :: Tree Bool
+subtree12 = Leaf False subtree13 subtree14
 
-subtree1 :: Tree Int
-subtree1 = Leaf 2 Empty Empty
+subtree13 :: Tree Bool
+subtree13 = Leaf False Empty Empty
 
-tree :: Tree Int
-tree = Leaf 1 subtree1 subtree2
+subtree14 :: Tree Bool
+subtree14 = Leaf True Empty Empty
 
+exercise1 = do
+ print "----- Arvore binaria de booleanos -----"
+ print tree1
+ print "---------------------------------------"
 
-main = print tree
+-- 2. Crie uma árvore em que cada nó guarda uma lista de inteiros.
+
+tree2 :: Tree [Int]
+tree2 = Leaf [1, 2] subtree21 Empty
+
+subtree21 :: Tree [Int]
+subtree21 = Leaf [3, 4] subtree22 subtree23
+
+subtree22 :: Tree [Int]
+subtree22 = Leaf [5, 6] Empty Empty
+
+subtree23 :: Tree [Int]
+subtree23 = Leaf [7, 8] Empty Empty
+
+exercise2 = do
+ print "----- Arvore binaria de lista de inteiros -----"
+ print tree2
+ print "-----------------------------------------------"
+
+-- MAIN
+main = do
+ exercise1
+ exercise2
